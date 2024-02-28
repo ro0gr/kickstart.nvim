afcb33a92c49de7c91c3d1ada9d37c0b86fa7406
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-02-28 11:46:56 +0100

    test colorschemes

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'onedark'
+    end,
+  },
+
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
+
+  {
+    'jacoborus/tender.vim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'tender'
+    end,
+  },
+
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'oxocarbon'
+    end,
+  },
+}

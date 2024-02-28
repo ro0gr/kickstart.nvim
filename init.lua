645eb03c9824bcdc81daf050d4adcd1a3a114399
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-02-28 13:44:01 +0100

    adjust frontend formatters

--- init.lua
+++ init.lua
@@ -653,7 +653,9 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { 'prettierd', 'eslintd' },
+        javascriptreact = { 'prettierd', 'eslintd' },
         css = { 'stylelint' },
+        scss = { 'stylelint', 'prettierd' },
       },
     },
   },

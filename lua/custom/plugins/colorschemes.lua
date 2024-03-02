380dc36afb380d76f02eed3d9fa381e2a2b49356
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-03-02 23:51:20 +0100

    add caret colorscheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -39,4 +39,12 @@ return {
       -- vim.cmd.colorscheme 'oxocarbon'
     end,
   },
+
+  {
+    'projekt0n/caret.nvim',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'caret'
+    end,
+  },
 }

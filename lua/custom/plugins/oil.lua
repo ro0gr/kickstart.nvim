b23e8b622963ade31d2262cb5fd0f5d434708041
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-03-06 13:04:31 +0100

    show hidden files in file explorer

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,6 +1,13 @@
 return {
   'stevearc/oil.nvim',
-  opts = {},
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('oil').setup {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    }
+  end,
 }

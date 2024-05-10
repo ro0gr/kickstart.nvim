b4c751ce2d3a4b26c62416b246f12ae3b7171715
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-05-10 11:42:20 +0200

    enable search refine

--- init.lua
+++ init.lua
@@ -394,12 +394,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<C-f>'] = 'to_fuzzy_refine' },
           },
+        },
         pickers = {
           find_files = {
             find_command = {

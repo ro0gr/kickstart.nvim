6a90e0531043a272172b04520454a9fd3edde377
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-05-17 11:59:09 +0200

    fix mapping for a new term

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -1,15 +1,15 @@
 return {
   'voldikss/vim-floaterm',
   config = function()
-    vim.keymap.set('n', '<M-`>', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<M-`>', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.keymap.set('n', '<M-`>', ':FloatermToggle<CR>', { silent = true })
+    vim.keymap.set('t', '<M-`>', '<C-\\><C-n>:FloatermToggle<CR>', { silent = true })
     -- for safety, allow to delete the terminal buffer only from the normal mode
-    vim.keymap.set('n', '<M-Del>', ':FloatermKill<CR>')
-    vim.keymap.set('n', '<M-Tab>', ':FloatermNext<CR>')
-    vim.keymap.set('t', '<M-Tab>', '<C-\\><C-n>:FloatermNext<CR>')
-    vim.keymap.set('n', '<M-S-Tab>', ':FloatermPrev<CR>')
-    vim.keymap.set('t', '<M-S-Tab>', '<C-\\><C-n>:FloatermPrev<CR>')
-    vim.keymap.set('n', '<M-S-=>', ':FloatermNew<CR>')
-    vim.keymap.set('t', '<M-S-=>', '<C-\\><C-n>:FloatermNew<CR>')
+    vim.keymap.set('n', '<M-Del>', ':FloatermKill<CR>', { silent = true })
+    vim.keymap.set('n', '<M-Tab>', ':FloatermNext<CR>', { silent = true })
+    vim.keymap.set('t', '<M-Tab>', '<C-\\><C-n>:FloatermNext<CR>', { silent = true })
+    vim.keymap.set('n', '<M-S-Tab>', ':FloatermPrev<CR>', { silent = true })
+    vim.keymap.set('t', '<M-S-Tab>', '<C-\\><C-n>:FloatermPrev<CR>', { silent = true })
+    vim.keymap.set('n', '<M-\\>', ':FloatermNew<CR>', { silent = true })
+    vim.keymap.set('t', '<M-\\>', '<C-\\><C-n>:FloatermNew<CR>', { silent = true })
   end,
 }

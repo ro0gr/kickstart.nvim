b9a9fda25cbd7de3f53696fff2fbe6ff58cfb957
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-05-17 12:41:49 +0200

    [g]it [b]ranches
     + rename blame to [g]it [B]lame

--- init.lua
+++ init.lua
@@ -467,6 +467,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = '[G]it [b]ranches' })
     end,
   },
 

b03160c305ff9b01d8fc2a0e8ef4e5563ae0125b
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-05-17 11:33:32 +0200

    re-enable LSP for project.nvim

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -3,11 +3,10 @@ return {
     'ahmedkhalf/project.nvim',
     event = 'VeryLazy',
     opts = {
-      detection_methods = { 'pattern' },
+      detection_methods = { 'lsp', 'pattern' },
       ignore_lsp = { 'lua_ls' },
       patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package-lock.json', '.sln' },
       ---@usage When set to false, you will get a message when project.nvim changes your directory.
-      -- When set to false, you will get a message when project.nvim changes your directory.
       silent_chdir = false,
     },
     config = function(_, opts)

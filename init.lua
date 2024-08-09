75f2b70fcb60fd7f6afd6c618954461dc4638395
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-08-09 11:19:53 +0200

    integrate telescope smart-open

--- init.lua
+++ init.lua
@@ -447,7 +447,6 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- my keymaps
       vim.keymap.set('n', '<leader>sp', require('telescope').extensions.projects.projects, { desc = '[S]earch [P]rojects' })
---
+++ lua/custom/plugins/telescope-extras.lua
@@ -0,0 +1,32 @@
+return {
+  -- {
+  -- 'prochri/telescope-all-recent.nvim',
+  -- dependencies = {
+  --   'nvim-telescope/telescope.nvim',
+  --   'kkharji/sqlite.lua',
+  --   -- optional, if using telescope for vim.ui.select
+  --   'stevearc/dressing.nvim',
+  -- },
+  -- opts = {
+  --   -- your config goes here
+  -- },
+  -- },
+  {
+    'danielfalk/smart-open.nvim',
+    branch = '0.2.x',
+    config = function()
+      require('telescope').load_extension 'smart_open'
+
+      vim.keymap.set('n', '<leader><leader>', function()
+        require('telescope').extensions.smart_open.smart_open()
+      end, { noremap = true, silent = true })
+    end,
+    dependencies = {
+      'kkharji/sqlite.lua',
+      -- Only required if using match_algorithm fzf
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { 'nvim-telescope/telescope-fzy-native.nvim' },
+    },
+  },
+}

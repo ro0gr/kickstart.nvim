f19a704f428debadcd09144dcb89fbd8f7af99db
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-08-09 11:01:48 +0200

    prevent the statusline plugin to corrupt the laststatus

--- init.lua
+++ init.lua
@@ -945,7 +945,11 @@ require('lazy').setup({
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        -- avoid mutation the laststatus
+        set_vim_settings = false,
+      }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for

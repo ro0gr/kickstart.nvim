dcd91f3b8365511f0b4fa7742952f0d18ba43a5c
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-08-23 00:00:01 +0200

    adjust git mappings + git log view

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -2,16 +2,29 @@ return {
   {
     'tpope/vim-fugitive',
     config = function()
-      vim.keymap.set('n', '<leader>gs', '<CMD>vertical Git<CR>', { desc = 'Git status' })
-      vim.keymap.set('n', '<leader>gB', '<CMD>Git blame<CR>', { desc = 'Git blame' })
-      vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = 'Git commit' })
-      vim.keymap.set('n', '<leader>gd', '<CMD>Git diff<CR>', { desc = 'Git diff' })
-      vim.keymap.set('n', '<leader>gl', '<CMD>Git log<CR>', { desc = 'Git log' })
-      vim.keymap.set('n', '<leader>gp', '<CMD>Git push<CR>', { desc = 'Git push' })
-      vim.keymap.set('n', '<leader>gr', '<CMD>Git rebase<CR>', { desc = 'Git rebase' })
-      vim.keymap.set('n', '<leader>gw', '<CMD>Git write<CR>', { desc = 'Git write' })
+      vim.keymap.set('n', '<leader>gc', '<CMD>tab Git commit<CR>', { desc = '[G]it commit' })
+      vim.keymap.set('n', '<leader>gac', '<CMD>tab Git commit --amend<CR>', { desc = '[G]it [a]mend [c]ommit' })
+      vim.keymap.set('n', '<leader>gB', '<CMD>Gitsigns blame<CR>', { desc = '[G]it [b]lame' })
+      vim.keymap.set('n', '<leader>gl', '<CMD>Flog<CR>', { desc = '[G]it [l]og' })
+      vim.keymap.set('n', '<leader>gp', '<CMD>Git push<CR>', { desc = '[G]it [p]ush' })
+      vim.keymap.set('n', '<leader>gr', '<CMD>Git rebase<CR>', { desc = '[G]it [r]ebase' })
+      vim.keymap.set('n', '<leader>gw', '<CMD>Git write<CR>', { desc = '[G]it [w]rite' })
     end,
   },
   'tommcdo/vim-fubitive',
   'tpope/vim-rhubarb',
+  {
+    'sindrets/diffview.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', '<CMD>DiffviewOpen<CR>', { desc = '[G]it [s]tatus' })
+    end,
+  },
+  {
+    'rbong/vim-flog',
+    lazy = true,
+    cmd = { 'Flog', 'Flogsplit', 'Floggit' },
+    dependencies = {
+      'tpope/vim-fugitive',
+    },
+  },
 }

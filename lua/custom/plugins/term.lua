97baf754ef6da96e6f8b2b72b2e25c25edbedb02
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-08-26 01:28:39 +0200

    customize floatterm

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -2,6 +2,7 @@ return {
   'voldikss/vim-floaterm',
   config = function()
     vim.keymap.set('n', '<M-`>', ':FloatermToggle<CR>', { silent = true })
+
     vim.keymap.set('t', '<M-`>', '<C-\\><C-n>:FloatermToggle<CR>', { silent = true })
     -- for safety, allow to delete the terminal buffer only from the normal mode
     vim.keymap.set('n', '<M-Del>', ':FloatermKill<CR>', { silent = true })
@@ -11,5 +12,10 @@ return {
     vim.keymap.set('t', '<M-S-Tab>', '<C-\\><C-n>:FloatermPrev<CR>', { silent = true })
     vim.keymap.set('n', '<M-\\>', ':FloatermNew<CR>', { silent = true })
     vim.keymap.set('t', '<M-\\>', '<C-\\><C-n>:FloatermNew<CR>', { silent = true })
+
+    -- FloatermNew --wintype=split --position=botright --height=15
+    vim.g.floaterm_wintype = 'split'
+    vim.g.floaterm_position = 'botright'
+    vim.g.floaterm_height = 15
   end,
 }

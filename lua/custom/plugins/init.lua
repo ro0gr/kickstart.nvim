96f19bc812ab4973de2083f7bbdafa9307000f6d
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-08-26 01:28:00 +0200

    active win
     + some more basic plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,61 @@
+vim.cmd 'cab Q q'
+vim.cmd 'cab W w'
+vim.cmd 'cab Wq wq'
+vim.cmd 'cab Qa qa'
+vim.cmd 'cab git Git'
+vim.cmd 'cab g Git'
+
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+  desc = 'extra customizations for terminal buffers',
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
+-- Highlight current line only on focused window
+vim.api.nvim_create_autocmd('WinLeave', {
+  desc = 'Hide cursor line when leaving window',
+  callback = function()
+    vim.opt.cursorline = false
+  end,
+})
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  desc = 'Display cursor line when entering window',
+  callback = function()
+    vim.opt.cursorline = true
+  end,
+})
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nanozuki/tabby.nvim',
+    -- event = 'VimEnter', -- if you want lazy load, see below
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('tabby').setup()
+    end,
+  },
+  {
+    'olrtg/nvim-emmet',
+    config = function()
+      vim.keymap.set({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+    end,
+  },
+  {
+    'psliwka/vim-dirtytalk',
+    build = ':DirtytalkUpdate',
+    config = function()
+      vim.opt.spelllang = { 'en', 'programming' }
+    end,
+  },
+
+  { 'miversen33/sunglasses.nvim', opts = {
+    filter_type = 'TINT',
+    filter_percent = 0.05,
+  }, config = true },
+}

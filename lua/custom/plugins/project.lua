7b899d18127b33166177346268c941c42de1796e
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2024-09-27 16:50:40 +0200

    project: fix floating cwd issue in tabs
    when working with multiple projects

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -3,11 +3,12 @@ return {
     'ahmedkhalf/project.nvim',
     event = 'VeryLazy',
     opts = {
-      detection_methods = { 'lsp', 'pattern' },
+      detection_methods = { 'pattern', 'lsp' },
       ignore_lsp = { 'lua_ls' },
       patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package-lock.json', '.sln' },
       ---@usage When set to false, you will get a message when project.nvim changes your directory.
       silent_chdir = false,
+      scope_chdir = 'tab',
     },
     config = function(_, opts)
       require('project_nvim').setup(opts)

c972ff0741c9a675398cd296e5f8a5a7fa6d4f0a
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-01-31 18:53:45 +0100

    show outline on the right

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -128,6 +128,9 @@ return {
       { '<leader>o', '<cmd>Outline<CR>', desc = 'Toggle outline' },
     },
     opts = {
+      outline_window = {
+        position = 'left',
+      },
       -- Your setup opts here
     },
   },

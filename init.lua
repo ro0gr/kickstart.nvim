f9d9f2af2e7cb3890fa786ac98a7730c1b378c04
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-02-15 23:24:09 +0100

    smarter ts_ls lang server enable logic

--- init.lua
+++ init.lua
@@ -681,7 +681,11 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        ts_ls = {
+          root_dir = require('lspconfig').util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json'),
+
+          single_file_support = false,
+        },
 
         html = { filetypes = { 'html', 'twig' } },
         ember = {},

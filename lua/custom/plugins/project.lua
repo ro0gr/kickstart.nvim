b16a4979453bd45894bdf557e2187197ad7ba051
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-02-15 23:30:49 +0100

    project: disable detection by LSP

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -7,7 +7,10 @@ return {
     },
     event = 'VeryLazy',
     opts = {
-      detection_methods = { 'pattern', 'lsp' },
+      detection_methods = {
+        'pattern',
+        -- 'lsp'
+      },
       ignore_lsp = { 'lua_ls' },
       patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package-lock.json', '.sln' },
       ---@usage When set to false, you will get a message when project.nvim changes your directory.

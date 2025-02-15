6bc5e5a470aebcc08b09b36502577ddd92136ee6
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-02-15 23:39:22 +0100

    neovide

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,32 @@
+if vim.g.neovide then
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  vim.g.neovide_text_gamma = 0.0
+  vim.g.neovide_text_contrast = 0.5
+
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.g.neovide_scale_factor = 1.2
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set('n', '<D-=>', function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set('n', '<D-->', function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
+
 vim.cmd 'cab Q q'
 vim.cmd 'cab W w'
 vim.cmd 'cab Wq wq'

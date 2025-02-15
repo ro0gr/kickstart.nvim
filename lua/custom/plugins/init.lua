18f0e4eae40cc5984f23ec4285e3b58d5f90b946
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-02-15 23:37:01 +0100

    error prone cwd color in statusline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -70,6 +70,14 @@ return {
     'hoob3rt/lualine.nvim',
     event = 'VimEnter',
     config = function()
+      local function if_int_to_hex(maybe_rgb)
+        if type(maybe_rgb) == 'number' then
+          return string.format('#%06x', maybe_rgb)
+        end
+        return maybe_rgb
+      end
+
+      local cwd_hl = vim.api.nvim_get_hl(0, { name = 'Title' })
       require('lualine').setup {
         -- extensions = { 'oil' },
         extensions = { 'fugitive', 'quickfix', 'lazy', 'mason' },
@@ -80,7 +88,11 @@ return {
               function()
                 return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
               end,
-              color = { fg = '#ffffff', gui = 'bold' },
+              color = {
+                fg = if_int_to_hex(cwd_hl.fg),
+                bg = if_int_to_hex(cwd_hl.bg),
+                gui = 'bold',
+              },
             },
 
             'branch',

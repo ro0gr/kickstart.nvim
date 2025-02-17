980dd97626085cd0aaaa15784b67b5c059780ca0
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2025-02-18 00:57:55 +0100

    Add keymap for the `TSContextToggle`

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -78,6 +78,7 @@ local function zzAndToggleScrolloff()
 end
 
 vim.keymap.set('n', 'zZ', zzAndToggleScrolloff, { noremap = true })
+vim.keymap.set('n', '<Leader>Tc', ':TSContextToggle<CR>', { noremap = true })
 
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)

6f009414303823fa3c410e6ab44c9bec7371c776
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-02-27 22:55:16 +0100

    disable sunglasses for now

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -59,9 +59,12 @@ return {
   --  + also https://github.com/TaDaa/vimade
   {
     'miversen33/sunglasses.nvim',
+    enabled = false,
     opts = {
       filter_type = 'SHADE',
       filter_percent = 0.1,
     },
+    config = true,
+    event = 'UIEnter',
   },
 }

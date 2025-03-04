d05bbb630ae8c8da3afe34fd4a3e6934b6f9d543
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-03-04 23:39:21 +0100

    extract neovide module

---
+++ lua/custom/plugins/neovide.lua
@@ -0,0 +1,45 @@
+if vim.g.neovide then
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  vim.g.neovide_text_gamma = 0.0
+  vim.g.neovide_text_contrast = 0.5
+
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.g.neovide_scale_factor = 1.2
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set('n', '<D-=>', function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set('n', '<D-->', function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
+
+return {
+  'beauwilliams/focus.nvim',
+  config = function()
+    require('focus').setup {
+      enable = true, -- Enable the plugin
+      ui = {
+        hybridnumber = false, -- Display hybrid line numbers in the focussed window only
+        -- absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
+
+        -- cursorline = true, -- Display a cursorline in the focussed window only
+        -- cursorcolumn = true, -- Display cursorcolumn in the focussed window only
+        -- winhighlight = true, -- auto highlighting for focussed/unfocussed windows
+      },
+    }
+  end,
+}

0795819225f6732332870fae2b1e51bc739b441c
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-03-13 13:05:20 +0100

    do not break `g//` command

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -17,7 +17,8 @@ vim.api.nvim_create_user_command('GitTab', FugitiveTab, {
 })
 
 vim.cmd 'cab G GitTab'
-vim.cmd 'cab g Git'
+vim.cmd 'cab gt GitTab'
+vim.cmd 'cab gi Git'
 vim.cmd 'cab gs Git switch'
 vim.cmd 'cab gsc Git switch -c '
 vim.cmd 'cab gpl Git pull origin '

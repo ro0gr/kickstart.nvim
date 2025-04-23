b8456091f8039f524aaa2e458db4bfd8e5193bb7
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-04-23 14:22:00 +0200

    stack based tabs

--- lua/custom/plugins/tabs.lua
+++ lua/custom/plugins/tabs.lua
@@ -4,6 +4,20 @@ vim.cmd 'cab tw tabedit $PWD'
 vim.cmd 'cab tr TabRename'
 vim.cmd 'cab tm tabmove'
 
+-- Stack based tabs
+--
+-- By default when ckosing a tab, it will switch to the next one
+-- So, say, if I open a new tab, then close it, I'll end up in a tab which is
+-- different from the one I was in initially. This is not the behavior I want.
+--
+-- It looks like the simpliest way to achieve this is to move open a new tab
+-- before the current one each time tabnew or tabedit is called.
+vim.api.nvim_create_autocmd('TabNew', {
+  callback = function()
+    vim.cmd 'tabmove -1'
+  end,
+})
+
 return {
   {
     'nanozuki/tabby.nvim',

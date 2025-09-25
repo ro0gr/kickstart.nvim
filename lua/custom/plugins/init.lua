5b0d4bf92d7cc9ec11a7739b4b4c9c548fab7dff
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-09-25 09:16:45 +0200

    global node version detection(node_host_prog)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,21 @@
+local function get_node_handle()
+  local handle = io.popen 'asdf which nodejs 2>/dev/null || which node'
+  if handle == nil then
+    print 'Warning: Could not open a shell to detect Node.js path. Plugins requiring Node.js may not work.'
+    return
+  end
+  local node_path = handle:read('*a'):gsub('\n', '')
+  handle:close()
+  return node_path
+end
+
+local node_path = get_node_handle()
+if node_path ~= '' then
+  vim.g.node_host_prog = node_path
+else
+  print 'Warning: Could not detect Node.js path. Plugins requiring Node.js may not work.'
+end
+
 vim.o.title = true
 
 local function update_title()

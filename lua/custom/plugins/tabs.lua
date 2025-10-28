b5db49fca467288cbf7bb25a33c3d21840fd2b49
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-10-28 08:49:06 +0100

    hotkeys to access tabs from 1 to 3

--- lua/custom/plugins/tabs.lua
+++ lua/custom/plugins/tabs.lua
@@ -3,4 +3,8 @@ vim.cmd 'cab te tabedit %:p:h'
 vim.cmd 'cab tw tabedit $PWD'
 vim.cmd 'cab tm tabmove'
 
+vim.keymap.set({ 'n', 'i', 't' }, '<M-1>', '1gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 't' }, '<M-2>', '2gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 't' }, '<M-3>', '3gt', { noremap = true, silent = true })
+
 return {}

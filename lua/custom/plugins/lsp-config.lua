344beb9fbbf9c13362d7c7734631b895c2daf87c
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-11-07 11:37:03 +0100

    lsp: do not highlight symbol if capability not supported

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -81,7 +81,15 @@ return {
         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
           buffer = event.buf,
           group = highlight_augroup,
-          callback = vim.lsp.buf.document_highlight,
+          callback = function()
+            local clients = vim.lsp.get_clients { bufnr = event.buf }
+            for _, client in ipairs(clients) do
+              if client.server_capabilities.documentHighlightProvider then
+                vim.lsp.buf.document_highlight()
+                return
+              end
+            end
+          end,
         })
 
         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {

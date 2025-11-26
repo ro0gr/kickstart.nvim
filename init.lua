22afd5c2fa8d6617cec55316046205cf159ad2a1
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-11-26 23:00:02 +0100

    maybe I don't need vim-sleuth?

--- init.lua
+++ init.lua
@@ -168,9 +168,6 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle

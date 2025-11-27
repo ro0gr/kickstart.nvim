6100ef609229e33bb60a65d18cbd3c881bc7da08
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-11-28 00:30:07 +0100

    migrate favourite colorschemes

---
+++ plugin/colorschemes.lua
@@ -0,0 +1,14 @@
+vim.pack.add {
+  'https://github.com/NLKNguyen/papercolor-theme',
+  'https://github.com/altercation/vim-colors-solarized',
+  'https://github.com/ellisonleao/gruvbox.nvim',
+  'https://github.com/projekt0n/caret.nvim',
+  'https://github.com/rakr/vim-one',
+  'https://github.com/rebelot/kanagawa.nvim',
+  'https://github.com/sainnhe/edge',
+  'https://github.com/sainnhe/everforest',
+  'https://github.com/savq/melange-nvim',
+  'https://github.com/zenbones-theme/zenbones.nvim',
+  { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
+  { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
+}

a1ca70ea1850e97e4df025bd77b4ec8ea35174ba
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-05 01:38:05 +0100

    built-in autocomplete

--- init.lua
+++ init.lua
@@ -95,6 +95,8 @@ vim.opt.foldnestmax = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.opt.completeopt:append 'fuzzy,noselect'
+vim.opt.completeopt:append 'preview'
 
 vim.keymap.set('n', '<M-->', '<CMD>e %:p:h<CR>', { desc = 'Open parent directory' })
 

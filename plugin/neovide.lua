972a94b7496a76fe327cb041b749e3a1204c023d
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-18 00:10:42 +0100

    neovide

---
+++ plugin/neovide.lua
@@ -0,0 +1,23 @@
+if vim.g.neovide then
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  vim.g.neovide_text_gamma = 0.0
+  vim.g.neovide_text_contrast = 0.5
+
+  vim.g.neovide_scale_factor = 1.2
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set('n', '<D-=>', function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set('n', '<D-->', function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
+
+return {}

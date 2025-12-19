e79a63513a115c7c61f4ad8a00dec59383ba787f
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-20 00:42:41 +0100

    find_to_split

--- plugin/grep.lua
+++ plugin/grep.lua
@@ -4,6 +4,7 @@ if vim.fn.executable 'rg' == 1 then
   vim.o.grepformat = '%f:%l:%c:%m'
 end
 
+-- type: `:sg<Space>` to use `:grep`
 vim.cmd 'cabbrev sg grep'
 vim.cmd 'cabbrev sga grepadd'
 vim.cmd 'cabbrev sgw grep -w'

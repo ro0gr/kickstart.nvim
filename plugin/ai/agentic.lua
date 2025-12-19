a738424b0c92566f5fabfa2218dcee52925b1f20
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-20 00:40:53 +0100

    agentic.nvim

---
+++ plugin/ai/agentic.lua
@@ -0,0 +1,19 @@
+vim.pack.add {
+  'https://github.com/carlos-algms/agentic.nvim',
+}
+
+require('agentic').setup {
+  provider = 'opencode-acp',
+}
+
+vim.keymap.set({ 'n', 't' }, '<M-a>', function()
+  require('agentic').toggle()
+end, { desc = 'Toggle agentic' })
+
+vim.keymap.set({ 'n', 'v' }, 'ga', function()
+  return require('agentic').add_selection_or_file_to_context()
+end, { expr = true, desc = 'Add range to agentic([A]I)' })
+
+vim.keymap.set('n', '<Leader>as', function()
+  require('agentic').new_session()
+end, { desc = 'New [A]gentic [S]ession' })

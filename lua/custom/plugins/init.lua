4db661983b2c7f2cad8f44b3b8e91169da912152
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-19 01:15:33 +0100

    cleanup more old config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,42 +17,8 @@ end, { noremap = true, silent = true })
 -- redo last command
 vim.keymap.set('n', '<M-.>', ':normal! @:<CR>', { noremap = true, silent = true })
 
---- Center screen and toggle scrolloff
-local orig_scrolloff
-
-local function zzAndToggleScrolloff()
-  if orig_scrolloff == nil then
-    orig_scrolloff = vim.opt.scrolloff:get()
-  end
-
-  vim.cmd 'normal! zz'
-
-  if 9999 == vim.opt.scrolloff:get() then
-    vim.notify 'Setting scrolloff to original value'
-    vim.opt.scrolloff = orig_scrolloff
-    vim.opt.cursorline = true
-    -- vim.opt.number = true
-    -- vim.opt.relativenumber = true
-  else
-    vim.notify 'Setting scrolloff to 9999'
-    vim.opt.scrolloff = 9999
-    vim.opt.cursorline = false
-    -- vim.opt.number = false
-    -- vim.opt.relativenumber = true
-  end
-end
-
-vim.keymap.set('n', 'zZ', zzAndToggleScrolloff, { noremap = true })
 vim.keymap.set('n', '<Leader>Tc', ':TSContextToggle<CR>', { noremap = true })
 
-vim.keymap.set('n', 'n', 'nzz', { noremap = true })
-vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
-vim.keymap.set('n', '*', '*zz', { noremap = true })
-vim.keymap.set('n', '#', '#zz', { noremap = true })
-vim.keymap.set('n', 'g*', 'g*zz', { noremap = true })
-vim.keymap.set('n', 'g#', 'g#zz', { noremap = true })
-
-vim.opt.guicursor:append 'ci-ve:block' -- Block cursor in command-line and replace modes
 vim.opt.wrap = false -- Disable line wrapping
 
 -- You can add your own plugins here or in other files in this directory!
@@ -60,14 +26,6 @@ vim.opt.wrap = false -- Disable line wrapping
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'psliwka/vim-dirtytalk',
-    build = ':DirtytalkUpdate',
-    config = function()
-      vim.opt.spelllang = { 'en', 'programming' }
-    end,
-  },
-
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
     config = function()
@@ -80,13 +38,5 @@ return {
         return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
       end
     end,
-  },
-
-  {
-    'brenoprata10/nvim-highlight-colors',
-    opts = {
-      render = 'virtual',
-      enable_named_colors = true,
-    },
   },
 }

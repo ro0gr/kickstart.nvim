9c96cc30353c34b2380b69986317b5c5287c84cb
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-31 00:40:57 +0100

    update agentic and others

--- plugin/ai/copilot.lua
+++ plugin/ai/copilot.lua
@@ -1,4 +1,7 @@
 -- TODO: try another plugin to be able to switch between models of different providers
+-- Consider using:
+-- - https://github.com/zbirenbaum/copilot.lua
+-- - https://github.com/copilotlsp-nvim/copilot-lsp
 
 vim.pack.add { 'https://github.com/github/copilot.vim' }
 

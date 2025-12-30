abe279aca9502c55a8f48d79f70af6f5c5578b2e
Ruslan Hrabovyi <kudgo.test@gmail.com>
2025-12-30 01:32:07 +0100

    improve agentic toggle

--- plugin/ai/agentic.lua
+++ plugin/ai/agentic.lua
@@ -2,20 +2,28 @@ vim.pack.add {
   'https://github.com/carlos-algms/agentic.nvim',
 }
 
-require('agentic').setup {
+local agentic = require 'agentic'
+
+agentic.setup {
   provider = 'opencode-acp',
 }
 
-vim.keymap.set({ 'n', 't' }, '<M-a>', function()
-  require('agentic').toggle()
-end, { desc = 'Toggle agentic' })
+-- focus the prompt input or close the chat if already focused
+vim.keymap.set({ 'n', 't', 'i' }, '<M-a>', function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local was_in_insert = vim.api.nvim_get_mode().mode == 'i'
 
-vim.keymap.set({ 'n', 'v' }, 'ga', function()
-  return require('agentic').add_selection_or_file_to_context()
+  agentic.open() -- open chat of gain focus
+
+  if vim.api.nvim_get_current_buf() == current_buf and was_in_insert then
+    -- if the buffer didn't change, it means we were already in the chat,
+    -- so let's treat this as a toggle
+    agentic.close()
+  end
+end, { desc = 'Toggle [a]gentic' })
+
+vim.keymap.set({ 'v' }, '<M-a>', function()
+  return agentic.add_selection()
 end, { expr = true, desc = 'Add range to agentic([A]I)' })
-
-vim.keymap.set('n', '<Leader>as', function()
-  require('agentic').new_session()
-end, { desc = 'New [A]gentic [S]ession' })
 
 vim.o.autoread = true

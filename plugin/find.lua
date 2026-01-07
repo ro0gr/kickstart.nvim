5a680a6d7ac83c6c886e33bc0ef23a93991eba58
Ruslan Hrabovyi <kudgo.test@gmail.com>
2026-01-07 01:05:46 +0100

    refactor(find): extract executable detection

--- plugin/find.lua
+++ plugin/find.lua
@@ -1,29 +1,36 @@
--- TODO:
---  - ctrl-q to send opened wildmenu completions to quickfix list.
-
 vim.cmd 'cabbrev sf find'
+
+local finders = {
+  'fd --type f --hidden --exclude .git',
+  'rg --files --hidden --glob "!.git/*"',
+}
+
+local detect_finder_cmd = function()
+  for _, cmd in pairs(finders) do
+    if vim.fn.executable(vim.split(cmd, ' ')[1]) == 1 then
+      return cmd
+    end
+  end
+
+  vim.notify('No file finder (fd, rg) found! Falling back to find.', vim.log.levels.WARN)
+
+  return 'find . -type f -not -path "*/\\.git/*" | sed "s|^\\./||"'
+end
+
+local finder_cmd = detect_finder_cmd()
 
 local find_complete_func = function(arg)
   -- Get git root or fallback to cwd
   local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
   local root = vim.v.shell_error == 0 and git_root or vim.fn.getcwd()
-  -- Use fd or find to get all files from root
-  local find_cmd
-  if vim.fn.executable 'fd' == 1 then
-    find_cmd = string.format('cd %s && fd --type f --hidden --exclude .git', vim.fn.shellescape(root))
-  elseif vim.fn.executable 'rg' == 1 then
-    find_cmd = string.format('cd %s && rg --files --hidden --glob "!.git/*"', vim.fn.shellescape(root))
-  else
-    find_cmd = string.format('cd %s && find . -type f -not -path "*/\\.git/*" | sed "s|^\\./||"', vim.fn.shellescape(root))
-  end
-  local all_files = vim.fn.systemlist(find_cmd)
 
+  local find = string.format('cd %s && ' .. finder_cmd, vim.fn.shellescape(root))
+  local all_files = vim.fn.systemlist(find)
   if arg == '' then
     return all_files
   end
 
-  local utils = require 'utils'
-  return utils.match_with_wildoptions(all_files, arg)
+  return require('utils').match_with_wildoptions(all_files, arg)
 end
 
 _G.FindFunct = find_complete_func

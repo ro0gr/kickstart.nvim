f4c8ffb558d71cf2b6eb096d18f70a41477b2c1f
Ruslan Hrabovyi <kudgo.test@gmail.com>
2026-01-16 23:45:08 +0100

    tabs: fix keymaps for `t` and `i` modes

--- plugin/tabs.lua
+++ plugin/tabs.lua
@@ -3,6 +3,11 @@ vim.cmd 'cab te tabedit %:p:h'
 vim.cmd 'cab tw tabedit $PWD'
 vim.cmd 'cab tm tabmove'
 
-vim.keymap.set({ 'n', 'i', 't' }, '<M-1>', '1gt', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 't' }, '<M-2>', '2gt', { noremap = true, silent = true })
-vim.keymap.set({ 'n', 'i', 't' }, '<M-3>', '3gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n' }, '<M-1>', '1gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n' }, '<M-2>', '2gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n' }, '<M-3>', '3gt', { noremap = true, silent = true })
+
+-- Terminal mode mappings
+vim.keymap.set({ 't', 'i' }, '<M-1>', '<C-\\><C-n>1gt', { noremap = true, silent = true })
+vim.keymap.set({ 't', 'i' }, '<M-2>', '<C-\\><C-n>2gt', { noremap = true, silent = true })
+vim.keymap.set({ 't', 'i' }, '<M-3>', '<C-\\><C-n>3gt', { noremap = true, silent = true })

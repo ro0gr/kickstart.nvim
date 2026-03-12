4d113b9a22f9bffc8a8867960540d5fb856d0a7d
Ruslan Hrabovyi <kudgo.test@gmail.com>
2026-03-12 11:31:45 +0100

    keybind for the last tab

--- plugin/tabs.lua
+++ plugin/tabs.lua
@@ -6,8 +6,10 @@ vim.cmd 'cab tm tabmove'
 vim.keymap.set({ 'n' }, '<M-1>', '1gt', { noremap = true, silent = true })
 vim.keymap.set({ 'n' }, '<M-2>', '2gt', { noremap = true, silent = true })
 vim.keymap.set({ 'n' }, '<M-3>', '3gt', { noremap = true, silent = true })
+vim.keymap.set({ 'n' }, '<M-4>', ':tablast<CR>', { noremap = true, silent = true })
 
 -- Terminal mode mappings
 vim.keymap.set({ 't', 'i' }, '<M-1>', '<C-\\><C-n>1gt', { noremap = true, silent = true })
 vim.keymap.set({ 't', 'i' }, '<M-2>', '<C-\\><C-n>2gt', { noremap = true, silent = true })
 vim.keymap.set({ 't', 'i' }, '<M-3>', '<C-\\><C-n>3gt', { noremap = true, silent = true })
+vim.keymap.set({ 't', 'i' }, '<M-4>', '<C-\\><C-n>:tablast<CR>', { noremap = true, silent = true })
